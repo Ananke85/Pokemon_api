ef4f527b9e9e898c134cd3c247a8609db36cc376
Lorena Carmona <126011228+Ananke85@users.noreply.github.com>
2024-05-27 18:23:41 +0200

    restoring files

--- Pokemon_backend/src/app.js
+++ Pokemon_backend/src/app.js
@@ -4,15 +4,15 @@ const express = require("express");
 const cors = require("cors");
 
 const blogRouter = require("./mongo/router/blogRouter");
-// const { connectDB } = require("./mongo/connection");
+const { connectDB } = require("./mongo/connection");
 
 const app = express();
 app.use(cors())
 app.use(express.json());
 app.use("/pokenews", blogRouter);
 
-// if (process.env.MONGO_URL) {
-//   connectDB().then(() => console.log("Connected to database!"));
-// }
+if (process.env.MONGO_URL) {
+  connectDB().then(() => console.log("Connected to database!"));
+}
 
 module.exports = app;
